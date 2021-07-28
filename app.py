98d8484902db56213fe5f3fd3db0f91d658df121
michpet2724 <michpet2724@gmail.com>
2021-07-28 14:35:28 +0000

    changed some .txt files

--- app.py
+++ app.py
@@ -9,8 +9,19 @@ import os
 app = Flask(__name__)
 
 
+app.config['GIPHY_KEY'] = "HPPlTs8kpN7jMHde8sQy9y0MYEGir6fL"
+KEY = app.config['GIPHY_KEY']
+
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods = ['GET', 'POST'])
+def display_gif():
+    print(request.form)
+    gif = request.form['gifchoice']
+    # url = getImageUrlFrom(gif, KEY)
+    # return render_template('yourgif.html', url = url)
+    return "I am alive"
